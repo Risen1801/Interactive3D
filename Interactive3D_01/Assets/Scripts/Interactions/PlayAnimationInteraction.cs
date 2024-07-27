c092d5d7c479422a058fe5f568ca2330e3d34ecd
MafackerJones <lust20035@hs-ansbach.de>
2024-07-27 16:15:12 +0200

    Adds and Discards
    - Deleted files that are unnecessary
    - Added Decals to Kitchen and Bathroom
    - Added extra Sounds to doors

--- Interactive3D_01/Assets/Scripts/Interactions/PlayAnimationInteraction.cs
+++ Interactive3D_01/Assets/Scripts/Interactions/PlayAnimationInteraction.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class PlayAnimationInteraction : InteractableObject
 {
     public Animator animator;
+    public AudioSource audioSource;
+    public AudioClip audioClipOpenDoor;
+    public AudioClip audioClipCloseDoor;
 
     private bool _isOpen;
+
+    private void Start()
+    {
+        audioSource.playOnAwake = false;
+    }
+
     public override void TriggerInteraction()
     {
         if (_isOpen)
@@ -17,15 +26,18 @@ public class PlayAnimationInteraction : InteractableObject
             TriggerOpenAnimation();
         }
     }
+
     public override void TriggerClosingAnimation()
     {
         animator.SetBool("isOpen", false);
         _isOpen = false;
+        audioSource.PlayOneShot(audioClipCloseDoor);
     }
 
     public override void TriggerOpenAnimation()
     {
         animator.SetBool("isOpen", true);
         _isOpen = true;
+        audioSource.PlayOneShot(audioClipOpenDoor);
     }
 }
